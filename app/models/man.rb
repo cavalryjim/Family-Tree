e3d585994329368ec8412c8389851a1ea7d88287
drypa <drypa@drypa-desktop.(none)>
2011-03-22 22:42:22 +0300

    for woman and man set default sex value

--- app/models/man.rb
+++ app/models/man.rb
@@ -1,2 +1,7 @@
 class Man < Human
+  before_save :default_values
+
+  def default_values
+    self.sex= 1
+  end
 end
--- app/models/woman.rb
+++ app/models/woman.rb
@@ -1,2 +1,7 @@
 class Woman < Human
+  before_save :default_values
+
+  def default_values
+    self.sex= 0
+  end
 end
